d68a3084fea301495120cf3c49ef5c1aa63e6fd4
yang <15177499516@163.com>
2018-11-24 14:20:08 +0800

    saving before ejecting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React from 'react';
+//import {render} from 'react-dom';
+//
+//import {HashRouter} from 'react-router-dom';
+//
+//import App from './components/App';
+//
+//render(
+//  <HashRouter>
+//      <App/>
+//  </HashRouter>,
+//  document.getElementById('app')
+//)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./css/index.css";
+ReactDOM.render(
+	<h1>你好!</h1>,
+	document.getElementById('app')
+);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render}from 'react-dom';
+import "./css/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((numbers) =>
+  <li key={numbers+1}>{numbers}</li>
+);
+render(
+	<ul>{listItems}</ul>,
+	document.getElementById('root')
+);
+
